f1885868db6e8363efac5f555e1783ac2bacb9a5
Matt Link <Linkm1993@gmail.com>
2020-02-21 22:14:41 -0500

    Changed the logic of script, re-worked html to be more clear, added more styling with css

--- script.js
+++ script.js
@@ -1,92 +1,46 @@
-let start = document.getElementsByClassName("start")
-let quizContainer = document.getElementsByClassName("quizcontainer")
-let questionDisplay = document.getElementsByClassName("question")
-let choiceA = document.getElementById("#A")
-let choiceB = document.getElementById("#B")
-let choiceC = document.getElementById("#C")
-let timeGauge = document.getElementsByClassName("timeguage")
+let startButton = document.getElementById("start-btn")
+let questionContainer = document.getElementsByClassName("question-container")
+let shuffleQs = 
 
+
+startButton.addEventListener('click', startGame);
+
+function startGame(){
+    console.log("Started")
+    startButton.classList.add('hide')
+    questionContainer[0].classList.remove('hide')
+    nextQuestion()
+}
+
+function nextQuestion(){
+
+}
+
+function selectAnswer(){
+
+
+}
 
 let questions = [
     {
-        question : "Anwser is A",
-        choiceA : "A",
-        choiceB : "B",
-        choiceC : "C",
+        question : "What does HTML stand for?",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
         correct : "A"
     },
     {
-        question : "Anwser B",
-        choiceA : "A",
-        choiceB : "B",
-        choiceC : "C",
+        question : "What does CSS stand for?",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
         correct : "B"
     },
     {
-        question : "Anwser C",
-        choiceA : "A",
-        choiceB : "B",
-        choiceC : "C",
+        question : "What does JS stand for?",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
         correct : "C"
     }
 ]
-
-let lastQuestionIndex = questions.length - 1;
-let runningQuestionIndex = 0;
-
-function renderQuestion(){
-    let q = questions[runningQuestionIndex];
-    questionDisplay.innerHTML = "<p>" + q.questionDisplay + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-
-
-}
-
-let questionTime = 10;
-let gaugeWidth = 150;
-let count = 0;
-let gaugeProgress = gaugeWidth/questionTime;
-let TIMER = setInterval(countRender, 1000);
-
-function countRender(){
-    if( count <= questionTime){
-        countRender.innerHTML = count;
-        document.getElementsByClassName("timeguage")[0].style.width = gaugeProgress * count + "px";
-        count++
-    }
-
-    else {
-        count = 0;
-        if(runningQuestionIndex < lastQuestionIndex){
-            runningQuestionIndex++
-            renderQuestion();
-        }
-
-        else {
-            clearInterval(TIMER);
-            scoreRender()
-        }
-
-        
-        }
-    }
-let score = 0;
-function checkAnwser(answer){
-    if(questions[runningQuestionIndex].correct == answer){
-        score++
-    }
-
-    else {
-        count = 0;
-        if(runningQuestionIndex < lastQuestionIndex){
-            runningQuestionIndex++
-            renderQuestion();
-        }
-        else{
-            clearInterval(TIMER);
-            scoreRender()
-        }
-    }
-}
