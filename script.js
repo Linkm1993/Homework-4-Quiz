057e6de76bab602611a171c6343b3c866373ae69
Matt Link <Linkm1993@gmail.com>
2020-02-21 23:44:03 -0500

    Pushing before sending for grade

--- script.js
+++ script.js
@@ -1,46 +1,112 @@
-let startButton = document.getElementById("start-btn")
-let questionContainer = document.getElementsByClassName("question-container")
-let shuffleQs = 
+let startButton = document.getElementById('start-btn')
+let nextButton = document.getElementById('next-btn')
+let questionContainerElement = document.getElementById('question-container')
+let questionElement = document.getElementById('question')
+let answerButtonsElement = document.getElementById('answer-buttons')
 
+let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-function startGame(){
-    console.log("Started")
-    startButton.classList.add('hide')
-    questionContainer[0].classList.remove('hide')
-    nextQuestion()
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-function nextQuestion(){
-
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer(){
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    let button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn', 'btn-primary')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
 
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
 
+function selectAnswer(e) {
+  let selectedButton = e.target
+  let correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 let questions = [
-    {
-        question : "What does HTML stand for?",
-        choiceA : "Correct",
-        choiceB : "Wrong",
-        choiceC : "Wrong",
-        correct : "A"
-    },
-    {
-        question : "What does CSS stand for?",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
-        correct : "B"
-    },
-    {
-        question : "What does JS stand for?",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
-        correct : "C"
-    }
+  {
+    question: 'The 1st choice is correct is correct',
+    answers: [
+      { text: 'Click here!', correct: true },
+      { text: 'Wrong', correct: false }
+    ]
+  },
+  {
+    question: 'Pick The Third choice',
+    answers: [
+      { text: 'Wrong', correct: false },
+      { text: 'Think again!', correct: false},
+      { text: 'Pick me!', correct: true },
+      { text: 'Not me!', correct: false}
+    ]
+  },
+  {
+    question: '2nd choice is correct',
+    answers: [
+      { text: 'Not here', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Nope', correct: false },
+      { text: 'Wrong!', correct: false }
+    ]
+  },
+  {
+    question: 'Pick the 2nd',
+    answers: [
+      { text: 'Wrong', correct: false },
+      { text: 'Pick me', correct: true }
+    ]
+  }
 ]
